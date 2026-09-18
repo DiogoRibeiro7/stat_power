50eff9b54e9647375b044e20619d268886bbf8c1
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-09-18 23:28:51 +0100

    feat: load numerical foundations

--- lib/stat_power.rb
+++ lib/stat_power.rb
@@ -2,6 +2,9 @@
 
 require_relative "stat_power/version"
 require_relative "stat_power/result"
+require_relative "stat_power/errors"
+require_relative "stat_power/distributions/normal"
+require_relative "stat_power/solvers/bisection"
 
 module StatPower
 end

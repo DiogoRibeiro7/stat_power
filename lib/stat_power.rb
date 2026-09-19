51e132ae8d90a2c9b6b0bb7f98529fbd6067a386
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-09-19 21:10:04 +0100

    feat: expose f2 power API

--- lib/stat_power.rb
+++ lib/stat_power.rb
@@ -5,6 +5,7 @@ require_relative "stat_power/result"
 require_relative "stat_power/power_result"
 require_relative "stat_power/unequal_power_result"
 require_relative "stat_power/anova_result"
+require_relative "stat_power/f2_result"
 require_relative "stat_power/errors"
 require_relative "stat_power/special_functions/beta"
 require_relative "stat_power/integration/adaptive_simpson"
@@ -22,6 +23,7 @@ require_relative "stat_power/t_test"
 require_relative "stat_power/t_test_unequal"
 require_relative "stat_power/correlation"
 require_relative "stat_power/anova"
+require_relative "stat_power/f2"
 
 module StatPower
 end

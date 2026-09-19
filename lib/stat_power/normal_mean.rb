47154a6dbf108f2ac331ee9dc4810ecf125cdabd
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-09-19 10:48:11 +0100

    style: simplify conventional effect lookup

--- lib/stat_power/normal_mean.rb
+++ lib/stat_power/normal_mean.rb
@@ -151,9 +151,7 @@ module StatPower
     def normalize_effect_size(value)
       return nil if value.nil?
 
-      if value.is_a?(String) || value.is_a?(Symbol)
-        return EffectSize::Conventional.resolve(test: :t, size: value)
-      end
+      return EffectSize::Conventional.resolve(test: :t, size: value) if value.is_a?(String) || value.is_a?(Symbol)
 
       Float(value)
     rescue ArgumentError, TypeError
